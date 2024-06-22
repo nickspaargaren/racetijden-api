060262f1319b756d323d5d483d5937373d2371fe
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-06-22 09:52:29 +0200

    Add getcircuits api response documentation

--- src/circuits/circuits.controller.ts
+++ src/circuits/circuits.controller.ts
@@ -1,13 +1,31 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { CircuitsService } from "./circuits.service";
 import { Circuits } from "@prisma/client";
-import { ApiBody } from "@nestjs/swagger";
+import { ApiBody, ApiResponse } from "@nestjs/swagger";
 
 @Controller("circuits")
 export class CircuitsController {
   constructor(private readonly circuitsService: CircuitsService) {}
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: "The found records",
+    schema: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          name: { type: "string" },
+          description: { type: "string" },
+          location: { type: "string" },
+          flag: { type: "string" },
+          createdAt: { type: "string", format: "date-time" },
+          id: { type: "number" },
+        },
+      },
+    },
+  })
   async getCircuits(): Promise<Circuits[]> {
     return this.circuitsService.getCircuits();
   }
@@ -36,7 +54,7 @@ export class CircuitsController {
       flag: string;
       createdAt: Date;
       id: number;
-    },
+    }
   ): Promise<Circuits> {
     return this.circuitsService.createCircuit(circuitData);
   }
